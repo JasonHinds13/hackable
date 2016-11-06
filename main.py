c42b65d4303e6d293356a4682765ed4ca4766f22
root <brandonhinds13@gmail.com>
2016-11-05 21:06:39 -0400

    Moved make database function to main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, os
 from flask import Flask, jsonify, render_template, g
 
 app = Flask(__name__)
@@ -35,4 +35,18 @@ def connect_db():
     return sqlite3.connect(app.database)
 
 if __name__ == "__main__":
+
+    #create database if it doesn't exist yet
+    if not os.path.exists('sample.db'):
+        with sqlite3.connect("sample.db") as connection:
+            c = connection.cursor()
+            c.execute("""CREATE TABLE shop_items(name TEXT, quantitiy TEXT, price TEXT)""")
+            c.execute("""CREATE TABLE employees(username TEXT, password TEXT)""")
+            c.execute('INSERT INTO shop_items VALUES("water", "40", "100")')
+            c.execute('INSERT INTO shop_items VALUES("juice", "40", "110")')
+            c.execute('INSERT INTO shop_items VALUES("candy", "100", "10")')
+            c.execute('INSERT INTO employees VALUES("itsjasonh", "badword")')
+            c.execute('INSERT INTO employees VALUES("theeguy9", "badpassword")')
+            c.execute('INSERT INTO employees VALUES("newguy29", "pass123")')
+
     app.run(host='0.0.0.0') #runs on machine ip address to make it visible on netowrk
--- static/script.js
+++ static/script.js
@@ -1,4 +1,17 @@
+//test our API
+$(document).ready(function(){
+    $.ajax('/api/v1.0/storeAPI',{
+        method:'GET'
+    }).done(function(res){
+        console.log(res);
+    }).fail(function(err){
+        console.log(err);
+    });
+});
+
 function search(){
     var loc = document.getElementById("searchItem").value;
     location.href= loc;
 }
+
+
